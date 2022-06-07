51228809f6b4cdbcdf3c04713c8419c4a5ee4a36
Joaobazzo <joao.bazzo@gmail.com>
2022-06-07 13:58:28 -0300

    update PNS and PNAd download links

--- R/1.0_download_PNS-PNAD.R
+++ R/1.0_download_PNS-PNAD.R
@@ -5,7 +5,7 @@ dir.create("../../data-raw/PNS/2019/")
 
 # PNS 2013----
 download.file(url = "https://ftp.ibge.gov.br/PNS/2013/Microdados/Dados/PNS_2013.zip",
-              destfile = "../../data-raw/PNS/2013/pns_2013.zip",mode = "wb")
+              destfile = "../../data-raw/PNS/2013/pns_2013_20200825.zip",mode = "wb")
 
 download.file(url = "https://ftp.ibge.gov.br/PNS/2013/Microdados/Documentacao/Chaves_PNS_2013.pdf",
               destfile = "../../data-raw/PNS/2013/Chaves_PNS_2013.pdf",mode = "wb")
@@ -21,8 +21,8 @@ download.file(url = "https://ftp.ibge.gov.br/PNS/2013/Microdados/Documentacao/Di
 
 # PNS 2019 -----
 
-download.file(url = "https://ftp.ibge.gov.br/PNS/2019/Microdados/Dados/PNS_2019_20210826.zip",
-              destfile = "../../data-raw/PNS/2019/pns_2019.zip",mode = "wb")
+download.file(url = "https://ftp.ibge.gov.br/PNS/2019/Microdados/Dados/PNS_2019_20220525.zip",
+              destfile = "../../data-raw/PNS/2019/pns_2019_20220525.zip",mode = "wb")
 
 download.file(url = "https://ftp.ibge.gov.br/PNS/2019/Microdados/Documentacao/Chaves_PNS_2019.pdf",
               destfile = "../../data-raw/PNS/2019/Chaves_PNS_2019.pdf",mode = "wb")
@@ -32,23 +32,23 @@ download.file(url = "https://ftp.ibge.gov.br/PNS/2019/Microdados/Documentacao/Di
 
 # PNAD 2008 ----
 
-download.file(url = "https://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_anual/microdados/reponderacao_2001_2009/PNAD_reponderado_2008.zip",
-              destfile = "../../data-raw/PNAD/pnad_2008.zip",mode = "wb")
+download.file(url = "https://ftp.ibge.gov.br/Trabalho_e_Rendimento/Pesquisa_Nacional_por_Amostra_de_Domicilios_anual/microdados/reponderacao_2001_2012/PNAD_reponderado_2008.zip",
+              destfile = "../../data-raw/PNAD/pnad_2008_20160817.zip",mode = "wb")
 
 # Extract-----
 # pns 2013
-unzip(zipfile = "../../data-raw/PNS/2013/pns_2013.zip",
-      exdir = "../../data-raw/PNS/2013")
+unzip(zipfile = "../../data-raw/PNS/2013/pns_2013_20200825.zip",
+      exdir = "../../data-raw/PNS/2013/pns_2013_20200825")
 unzip(zipfile = "../../data-raw/PNS/2013/Dicionario_e_input_2013.zip",
       exdir = "../../data-raw/PNS/2013")
 
 
 # pns 2019
-unzip(zipfile = "../../data-raw/PNS/2019/pns_2019.zip",
-      exdir = "../../data-raw/PNS/2019")
+unzip(zipfile = "../../data-raw/PNS/2019/pns_2019_20220525.zip",
+      exdir = "../../data-raw/PNS/2019/pns_2019_20220525")
 unzip(zipfile = "../../data-raw/PNS/2019/Dicionario_e_input_2019.zip",
       exdir = "../../data-raw/PNS/2019")
 
 # pnad
-unzip(zipfile = "../../data-raw/PNAD/pnad_2008.zip",
-      exdir = "../../data-raw/PNAD")
+unzip(zipfile = "../../data-raw/PNAD/pnad_2008_20160817.zip",
+      exdir = "../../data-raw/PNAD/2008/pnad_2008_20160817")
